b3a008420e99205537e8e51067512f471c509ddc
Hillariousice <107556065+Hillariousice@users.noreply.github.com>
2022-12-12 22:05:44 +0100

    Docker & Docker compose yml

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "./App.css";
 
-const App = () =>{
+const App = () => {
 	return (
 		<React.Fragment>
 			<Router>
 				<Routes>
-					<Route path="/" element={<Home/>} />
+					<Route path="/" element={<Home />} />
 				</Routes>
 			</Router>
 		</React.Fragment>
 	);
-}
+};
 
 export default App;
