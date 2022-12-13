0afdff05a6fb5f3b5ca45047871c5efb5cdab663
glorylaw <100371169+glorylaw@users.noreply.github.com>
2022-12-13 23:27:57 +0100

    login page design

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "./App.css";
+import Login from "./pages/logins/Login";
 
 const App = () =>{
 	return (
@@ -9,6 +10,7 @@ const App = () =>{
 			<Router>
 				<Routes>
 					<Route path="/" element={<Home/>} />
+					<Route path="/login" element={<Login/>} />
 				</Routes>
 			</Router>
 		</React.Fragment>
