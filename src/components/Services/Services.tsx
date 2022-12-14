c84c598d987d638c1a9c395b6adf573a9613ffbf
Chizoba Victory <chizoba.victory1@gmail.com>
2022-12-14 15:25:29 +0100

    Added services component

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -2,6 +2,9 @@ import "./Services.css";
 import reliable from "../../assets/reliable.svg";
 import time from "../../assets/time.svg";
 import track from "../../assets/track.svg";
+import headset from "../../assets/headset.svg";
+import world from "../../assets/world.svg";
+import order from "../../assets/order.svg";
 
 const Services = () => {
 	return (
@@ -37,6 +40,35 @@ const Services = () => {
 					<div className="card">
 						<img src={track} alt="track" />
 						<p className="cardTitle">Track your shipment</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante
+							maecenas fusce orci nullam aenean arcu tellus. Massa rhoncus
+							vestibulum, at nunc pellentesque ornare urna
+						</p>
+					</div>
+				</div>
+				<div className="column">
+					<div className="card">
+						<img src={headset} alt="headset" />
+						<p className="cardTitle">Great Customer Service</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante
+							maecenas fusce orci nullam aenean arcu tellus. Massa rhoncus
+							vestibulum, at nunc pellentesque ornare urna
+						</p>
+					</div>
+					<div className="card">
+						<img src={world} alt="world" />
+						<p className="cardTitle">Nationwide Delivery</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante
+							maecenas fusce orci nullam aenean arcu tellus. Massa rhoncus
+							vestibulum, at nunc pellentesque ornare urna
+						</p>
+					</div>
+					<div className="card">
+						<img src={order} alt="order" />
+						<p className="cardTitle">Order Fulfilment</p>
 						<p>
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante
 							maecenas fusce orci nullam aenean arcu tellus. Massa rhoncus
