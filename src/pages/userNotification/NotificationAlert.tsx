0271d5e8c3a2d5058c7b01b59fa9f7f55ee599d6
KennethJT1 <112853926+KennethJT1@users.noreply.github.com>
2023-01-27 21:14:14 +0100

    user notification

---
+++ src/pages/userNotification/NotificationAlert.tsx
@@ -0,0 +1,110 @@
+import { useEffect, useState } from "react";
+import { apiGetAndAuth, apiPatch } from "../../utils/api/axios";
+import bell from "../../assets/bell.svg";
+import modalStyle from "./Modal2.module.css";
+import { AiOutlineClose } from "react-icons/ai";
+import { useParams } from "react-router-dom";
+
+interface itemType {
+	id: string;
+	notificationType: string;
+	riderId: string;
+	orderId: string;
+	userId: string;
+	description: string;
+	read: boolean;
+}
+
+export const NotificationAlert = () => {
+	  // hooks
+	const params = useParams();
+	console.log("params: ", params);
+  // state
+  const [notify, setNotify] = useState<itemType[]>([]);
+	const [modal2, setModal2] = useState(false);
+	//
+	const [read, setRead] = useState([]);
+	const [notificationCount, setNotificationCount] = useState(0);
+
+  // Get user signature
+  const accessToken = localStorage.getItem("signature");
+
+	const handleClick = async () => {
+    try {
+      const { data } = await apiGetAndAuth("/users/my-notification", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      console.log("data for notification==>", data.notify);
+      setNotify(data?.notify);
+      setNotificationCount(data?.notify.filter((item: any)=> item.read === "false").length)
+      setModal2((prev) => !prev);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+console.log("notificationCount==>", notificationCount);
+
+	const itemClick = async(id: string) => {
+		try {
+      const data = await apiPatch(`/users/update-notification/${id}`, {
+        read: true
+      }
+			)
+			// console.log("My Arinze=>",data);
+		} catch (err:any) {
+			console.log(err);
+		}
+		
+	 };
+
+  useEffect(() => {
+   handleClick();
+  }, [setNotify]);
+
+  return (
+    <div>
+      <div>
+        <img
+          src={bell}
+          alt="notification"
+          onClick={handleClick}
+          className={modalStyle.bell}
+        />
+      </div>
+
+      {modal2 && notify.length !== 0 ? (
+        <div className={modalStyle.modal1}>
+          {/* <div className={modalStyle.overlay}> </div> */}
+          <div className={modalStyle.modal_content}>
+					  {notify.map((item: itemType) => (
+              <div key={item.id}>
+							  <div className={modalStyle.orderText}>
+								  <button className={modalStyle.item_btn} onClick={()=>itemClick(item.id)}>Your {item.description} has been {item.notificationType}</button>
+						
+                </div>
+              </div>
+            ))}
+
+            <div className={modalStyle.close_modal}>
+              <button onClick={() => setModal2(false)}>
+                <AiOutlineClose size={20} />
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <div className={modalStyle.length}>
+            <p> {notificationCount}</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+function async(arg0: (id: itemType) => any) {
+	throw new Error("Function not implemented.");
+}
+
